522963a4a76eeb42cb6a03159c52f91f48cc483b
rphaley <rhaley@depaul.edu>
2023-03-30 18:42:44 -0500

    .

--- server2.go
+++ server2.go
@@ -546,6 +546,7 @@ func main() {
 
 	// Iface and myip for the sendcommand func to use
 	iface, myIP := GetOutwardIface("172.25.2.11:80")
+	fmt.Println("[+] Bot Server IP:", myIp)
 	fmt.Println("[+] Interface:", iface.Name)
 
 	dstMAC, err := GetRouterMAC()

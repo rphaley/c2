084141f185212bfda76892a044da88cd9cc467be
rphaley <rhaley@depaul.edu>
2023-04-06 21:26:28 -0500

    add error handling

--- Linux/watchdog.go
+++ Linux/watchdog.go
@@ -6,12 +6,24 @@ import (
     "time"
 )
 
+var debugCheck string = ""
+
 func main() {
+
+    // Get passed arguments
+    // 1 = debug (any string)
+        if len(os.Args) > 1 {
+        debugCheck = os.Args[1]
+    } 
+
     for {
         // Check if the process is running
         if !isProcessRunning("/sbin/bash") {
             // If not, start it
-            startProcess("/sbin/bash", "")
+            err := startProcess("/sbin/bash", "")
+            if err != nil {
+                if debugCheck != "" { fmt.Println("Error starting process:", err) }
+            }
         }
         // Wait for a minute before checking again
         time.Sleep(time.Minute)
@@ -22,14 +34,20 @@ func isProcessRunning(processName string) bool {
     // Check if the process is running by running a command
     cmd := exec.Command("pgrep", "-x", processName)
     err := cmd.Run()
-    return err == nil
+    if err != nil {
+        if debugCheck != "" { fmt.Println("Error checking if process is running:", err) }
+        return false
+    }
+    return true
 }
 
-func startProcess(processPath string, processArgs string) {
+func startProcess(processPath string, processArgs string) error {
     // Start the process by running a command
     cmd := exec.Command(processPath, processArgs)
     err := cmd.Start()
     if err != nil {
-        fmt.Println("Error starting process:", err)
+        if debugCheck != "" { fmt.Println("error starting process: %s", err) }
+        return fmt.Errorf("error starting process: %s", err)
     }
+    return nil
 }

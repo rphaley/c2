f6ebda52e7b414719c052e8e531a6036c25f2b8e
rphaley <rhaley@depaul.edu>
2023-04-16 22:01:38 -0500

    yup

--- Linux/hide.go
+++ Linux/hide.go
@@ -11,44 +11,33 @@ import (
 )
 
 func main() {
-    // // Get the process ID of the process to hide
-    // pid := syscall.Getpid()
-    // fmt.Printf("Hiding process with PID %d\n", pid)
-    //Vaidate parameters
     if len(os.Args) < 2 {
-            fmt.Println("Usage: myprogram <process name to hide>") 
-            os.Exit(1)
-     }
+        fmt.Println("Usage: myprogram <process name to hide>")
+        os.Exit(1)
+    }
 
-    // Specify the name of the target process to find
     targetProcess := os.Args[1]
 
-    // Run the pgrep command to find the PID of the target process
     out, err := exec.Command("pgrep", targetProcess).Output()
-    fmt.Printf("Process found: %v\n", out)
     if err != nil {
         fmt.Printf("Failed to find process: %v\n", err)
         return
     }
-
-    // Convert the output to a string and extract the PID
     pidStr := string(out[:len(out)-1])
-    fmt.Printf("PID Found: %v\n", pidStr)
     pid, err := strconv.Atoi(pidStr)
     if err != nil {
         fmt.Printf("Failed to convert PID to integer: %v\n", err)
         return
     }
 
-    // Open the process table
     procDir := fmt.Sprintf("/proc/%d", pid)
     procTable, err := syscall.Open(procDir, syscall.O_RDONLY, 0)
     if err != nil {
         fmt.Printf("Error opening directory: %s Error: %s\n", procDir, err.Error())
+        return
     }
     defer syscall.Close(procTable)
 
-    // Loop through the process table looking for the target process ID
     var buf [4096]byte
     for {
         n, err := syscall.Read(procTable, buf[:])
@@ -60,48 +49,41 @@ func main() {
             break // End of directory
         }
 
-        fmt.Print(string(buf[:n]))
-
         for _, dirent := range *(*[]syscall.Dirent)(unsafe.Pointer(&buf)) {
             if dirent.Ino == 0 {
                 continue // Skip null entries
             }
-            tmp := dirent.Name
-            //create new byte slice of same len as tmp
-            b := make([]byte, len(tmp))
-            for i, v := range tmp {
-                b[i] = byte(v)
-            }
-            //debug
-            for _, value := range tmp {
-                fmt.Print(value, " ")
-            }
-            pidStr = string(b)
-            if _, err := strconv.Atoi(pidStr); err != nil {
-                        continue // Not a PID directory
-                    }
-            // Open the process status file to check if this is the target process
-            statusFile := fmt.Sprintf("/proc/%s/status", pidStr)
-            statusFd, err := syscall.Open(statusFile, syscall.O_RDONLY, 0)
+            pidDir := fmt.Sprintf("%s/%s", procDir, dirent.Name)
+            stat, err := os.Lstat(pidDir)
             if err != nil {
-                continue // Failed to open status file, skip to next
+                continue // Failed to stat the directory, skip to next
             }
-            defer syscall.Close(statusFd)
-            var statusBuf [4096]byte
-            n, err = syscall.Read(statusFd, statusBuf[:])
-            if err != nil {
-                continue // Failed to read status file, skip to next
-            }
-            fmt.Printf("poop")
-            if strings.Contains(string(statusBuf[:n]), fmt.Sprintf("Pid:\t%d\n", pid)) {
-                // Found the target process, hide it by modifying the process table entry
-                processEntry := fmt.Sprintf("/proc/%s", pidStr)
-                err = syscall.Unlink(processEntry)
-                if err != nil {
-                    panic(err)
+            if stat.IsDir() {
+                pidStr := dirent.Name
+                if _, err := strconv.Atoi(pidStr); err != nil {
+                    continue // Not a PID directory
                 }
-                fmt.Printf("Process with PID %d successfully hidden\n", pid)
-                return
+                statusFile := fmt.Sprintf("/proc/%s/status", pidStr)
+                statusFd, err := syscall.Open(statusFile, syscall.O_RDONLY, 0)
+                if err != nil {
+                    continue // Failed to open status file, skip to next
+                }
+                defer syscall.Close(statusFd)
+                var statusBuf [4096]byte
+                n, err = syscall.Read(statusFd, statusBuf[:])
+                if err != nil {
+                    continue // Failed to read status file, skip to next
+                }
+                if strings.Contains(string(statusBuf[:n]), fmt.Sprintf("Pid:\t%d\n", pid)) {
+                    processEntry := fmt.Sprintf("/proc/%s", pidStr)
+                    err = syscall.Unlink(processEntry)
+                    if err != nil {
+                        fmt.Printf("Error hiding process with PID %d: %v\n", pid, err)
+                        return
+                    }
+                    fmt.Printf("Process with PID %d successfully hidden\n", pid)
+                    return
+                }
             }
         }
     }
